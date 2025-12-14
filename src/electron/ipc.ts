7ac4ea05abd70d2fa20100638350c86c4344fa0e
lenid <kurtishov.lr@gmail.com>
2025-12-14 23:12:53 +0100

    fix backend once again

--- src/electron/ipc.ts
+++ src/electron/ipc.ts
@@ -1,29 +1,30 @@
 import { BrowserWindow, ipcMain, dialog } from "electron";
 import { loadTextFile, saveTextFile } from "./services/fileService.js";
 import { CPU } from "./compiler/cpu.js";
-import run from "./compiler/executer.js";
 import { parseProgram } from "./parser/parser.js";
+import run from "./compiler/executer.js";
 
 // тут безпосередньо прописуємо як бекенд має реагувати на кожний із івентів
-export function registerIPC(win: BrowserWindow) {
+export async function registerIPC(win: BrowserWindow) {
     const cpu = CPU.getInstance()
 
     // якийсьмодульбеку.setOnUpdateCallback(() => contents.send("computronUpdate", state)) - ліпше винести так аби вся ipc логіка лишилась тут і тільки тут
 
     // тут потрібне має підставить бекендер
-    ipcMain.on("compile", (evt, data: {
+    ipcMain.on("compile", async (evt, data: {
         plaintextCode: string;
         runAfterCompilation: boolean;  // Та, ви все правильно розумієте, ці типи прописуються окремо в трьох місцях, гарного дня, (#electron_typescript_<3_<3_<3)
     }) => {
         parseProgram(data.plaintextCode, cpu);
         if (data.runAfterCompilation) {
-            run(cpu);
+            await run(cpu);
         }
     });
 
-    ipcMain.on("run", (evt) => {
+    ipcMain.on("run", async (evt) => {
         console.log("Run program");
-        run(cpu);
+        await run(cpu);
+        console.log("Program has ended")
     });
 
     ipcMain.on("setRegister", (evt, data: {
